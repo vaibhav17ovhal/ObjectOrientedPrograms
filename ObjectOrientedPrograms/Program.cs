35384b555e199a1cdab0717124f3cfd51dfee401
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-09 09:20:29 +0530

    [Vaibhav] Add:Json Inventory Data Management of Rice,Pulses,Wheats

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to object oriented program");
+
+            Console.WriteLine("==========||==========");
+
+            string path = "C:\\Users\\Dell\\Desktop\\BL_Session\\ObjectOrientedPrograms\\ObjectOrientedPrograms\\Inventory.json";
+
+            ReadTheData read = new ReadTheData();
+
+            InventoryDetails data = read.Read(path);
+
+            Console.WriteLine("==========|Rice|==========");
+            for (int i = 0; i < data.typeOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typeOfRice[i].name);
+                Console.WriteLine(data.typeOfRice[i].weight);
+                Console.WriteLine(data.typeOfRice[i].price + "\n");
+            }
+            Console.WriteLine("==========|Pulses|==========");
+            for (int j =  0; j < data.typeOfWheats.Count; j++)
+            {
+                Console.WriteLine(data.typeOfPulses[j].name);
+                Console.WriteLine(data.typeOfPulses[j].weight);
+                Console.WriteLine(data.typeOfPulses[j].price + "\n");
+            }
+            Console.WriteLine("==========|Wheat|==========");
+            for ( int k = 0; k < data.typeOfPulses.Count; k++)
+            {
+                Console.WriteLine(data.typeOfWheats[k].name);
+                Console.WriteLine(data.typeOfWheats[k].weight);
+                Console.WriteLine(data.typeOfWheats[k].price + "\n");
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPrograms/ReadTheData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+namespace ObjectOrientedPrograms
+{
+    public class ReadTheData
+    {
+        public InventoryDetails Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
